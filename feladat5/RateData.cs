f6080a57c48dfd5ea830883e7d3c10119154b3fa
Róbert Vilmos Csizmadia <robert.csizmadia@stud.uni-corvinus.hu>
2022-10-19 18:25:08 +0200

    feladatok 1,2,3,4,5,6

---
+++ feladat5/RateData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        public DateTime Date { get; set; }
+		public string Currency { get; set; }
+		public decimal Value { get; set; }
+	}
+}
--- feladat5/feladat5/Form1.cs
+++ feladat5/feladat5/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using feladat5.Entities;
+using feladat5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace feladat5
 {
@@ -15,6 +19,80 @@ namespace feladat5
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+            XMLfeldolgozas();
+            AdatokDiagram();
+            dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates; 
         }
+
+        string result;
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+
+            var response = mnbService.GetExchangeRates(request);
+
+            
+            result = response.GetExchangeRatesResult;
+            richTextBox1.Text = result.ToString();
+            
+        }
+
+        private void XMLfeldolgozas()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+               
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        private void AdatokDiagram()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
     }
 }
