f410b37e44da4d4537d61dcdb44d044388f23c94
vou4pm <vou4pm@SZA0025797>
2022-11-04 10:43:05 +0100

    szinvalaszto gomb

--- feladat6/Form1.Designer.cs
+++ feladat6/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.lblNext = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -86,11 +87,23 @@
             this.lblNext.TabIndex = 3;
             this.lblNext.Text = "Coming next:";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Tomato;
+            this.button3.Location = new System.Drawing.Point(100, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.lblNext);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -111,6 +124,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button button3;
     }
 }
 
