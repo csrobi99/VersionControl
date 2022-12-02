a0fcb0eef23270f52e1393cfba3591a9b95775e1
vou4pm <vou4pm@SZA0025797>
2022-12-02 10:59:48 +0100

    szimulacio

--- feladat8/Form1.cs
+++ feladat8/Form1.cs
@@ -24,6 +24,26 @@ namespace feladat8
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for(int year =2005; year<=2024; year++)
+            {
+                for (int i=0; i<Population.Count; i++)
+                {
+
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                  where x.Gender == Gender.Female && x.IsAlive
+                                  select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+
+
+
+            }
         }
 
 
@@ -90,5 +110,7 @@ namespace feladat8
 
             return deathprobability;
         }
+
+        Random rng = new Random(1234);
     }
 }
